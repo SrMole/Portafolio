f40186ab2308dd72c531e467153e592a147e7c2f
SrMole <jmanzano1@ucol.mx>
2022-09-16 14:47:16 -0500

    cambio

---
+++ Clase06/main.py
@@ -0,0 +1,30 @@
+
+
+#instala todo esto pendejo
+#pip install streamlit
+#pip install pyttsx3
+#as es un alias
+
+#tkinter importa 
+#%%
+
+def suma(a,b):
+    return a+b
+
+print(suma(b=2,a=0))
+
+#%%
+
+def suma(a,b,c=0):
+    return a/b
+
+print(suma(2,4))
+
+#%%
+
+def suma(a=0,b=3,c=0):
+    return a+b+c
+
+print(suma(b=4))
+
+# %%
